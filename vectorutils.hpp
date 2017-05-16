33c219595a16690cf6384ecc4e9bc11fd227cb2e
Hilder Vitor Lima Pereira <hilder.vitor@gmail.com>
2017-05-16 14:55:50 +0200

    Now vector functions can receive vectors of different types.

--- vectorutils.hpp
+++ vectorutils.hpp
@@ -8,8 +8,8 @@
 
 using namespace std;
 
-template <typename ELEMENT>
-void operator+=(vector<ELEMENT>& u, const vector<ELEMENT>& v){
+template <typename T1, typename T2>
+void operator+=(vector<T1>& u, const vector<T2>& v){
 	if (v.size() != u.size())
 		throw std::invalid_argument("It is impossible to add vectors of different sizes.");
 	unsigned int n = u.size();
@@ -18,16 +18,16 @@ void operator+=(vector<ELEMENT>& u, const vector<ELEMENT>& v){
 	}
 }
 
-template <typename ELEMENT>
-void operator+=(vector<ELEMENT>& u, const ELEMENT& c){
+template <typename T1, typename T2>
+void operator+=(vector<T1>& u, const T2& c){
 	unsigned int n = u.size();
 	for (unsigned int i = 0; i < n; i++){
 		u[i] = u[i] + c;
 	}
 }
 
-template <typename ELEMENT>
-void operator-=(vector<ELEMENT>& u, const vector<ELEMENT>& v){
+template <typename T1, typename T2>
+void operator-=(vector<T1>& u, const vector<T2>& v){
 	if (v.size() != u.size())
 		throw std::invalid_argument("It is impossible to subtract vectors of different sizes.");
 	unsigned int n = u.size();
@@ -35,16 +35,17 @@ void operator-=(vector<ELEMENT>& u, const vector<ELEMENT>& v){
 		u[i] = u[i] - v[i];
 	}
 }
-template <typename ELEMENT>
-void operator-=(vector<ELEMENT>& u, const ELEMENT& c){
+
+template <typename T1, typename T2>
+void operator-=(vector<T1>& u, const T2& c){
 	unsigned int n = u.size();
 	for (unsigned int i = 0; i < n; i++){
 		u[i] = u[i] - c;
 	}
 }
 
-template <typename ELEMENT>
-void operator*=(vector<ELEMENT>& u, const ELEMENT& c){
+template <typename T1, typename T2>
+void operator*=(vector<T1>& u, const T2& c){
 	unsigned int n = u.size();
 	for (unsigned int i = 0; i < n; i++){
 		u[i] = u[i] * c;
@@ -59,44 +60,45 @@ void operator/=(vector<T1>& u, const T2& m){
 	}
 }
 
-template <typename ELEMENT>
-vector<ELEMENT> operator+(const vector<ELEMENT>& u, const vector<ELEMENT>& v){
-	vector<ELEMENT> vec(u);
+template <typename T1, typename T2>
+vector<T1> operator+(const vector<T1>& u, const vector<T2>& v){
+	vector<T1> vec(u);
 	vec += v;
 	return vec;
 }
 
-template <typename ELEMENT>
-vector<ELEMENT> operator+(const vector<ELEMENT>& u, const ELEMENT& c){
-	vector<ELEMENT> vec(u);
+template <typename T1, typename T2>
+vector<T1> operator+(const vector<T1>& u, const T2& c){
+	vector<T1> vec(u);
 	vec += c;
 	return vec;
 }
-template <typename ELEMENT>
-vector<ELEMENT> operator+(const ELEMENT& c, const vector<ELEMENT>& v){
+
+template <typename T1, typename T2>
+vector<T1> operator+(const T1& c, const vector<T2>& v){
 	return v + c; 
 }
 
-template <typename ELEMENT>
-vector<ELEMENT> operator-(const vector<ELEMENT>& u, const vector<ELEMENT>& v){
-	vector<ELEMENT> vec(u);
+template <typename T1, typename T2>
+vector<T1> operator-(const vector<T1>& u, const vector<T2>& v){
+	vector<T1> vec(u);
 	vec -= v;
 	return vec;
 }
 
-template <typename ELEMENT>
-vector<ELEMENT> operator-(const vector<ELEMENT>& u, const ELEMENT& c){
-	vector<ELEMENT> vec(u);
+template <typename T1, typename T2>
+vector<T1> operator-(const vector<T1>& u, const T2& c){
+	vector<T1> vec(u);
 	vec -= c;
 	return vec;
 }
 
-template <typename ELEMENT>
-ELEMENT operator*(const vector<ELEMENT>& u, const vector<ELEMENT>& v){
+template <typename T1, typename T2>
+T1 operator*(const vector<T1>& u, const vector<T2>& v){
 	if (u.size() != v.size())
 		throw std::invalid_argument("It is impossible to multiply vectors of different sizes.");
 	unsigned int n = u.size();
-	ELEMENT innerProduct(u[0] * v[0]);
+	T1 innerProduct(u[0] * v[0]);
 	for (unsigned int i = 1; i < n; i++){
 		innerProduct = innerProduct + u[i] * v[i];
 	}
@@ -104,25 +106,25 @@ ELEMENT operator*(const vector<ELEMENT>& u, const vector<ELEMENT>& v){
 	return innerProduct;
 }
 
-template <typename ELEMENT>
-vector<ELEMENT> operator*(const vector<ELEMENT>& u, const ELEMENT& c){
-	vector<ELEMENT> vec(u);
+template <typename T1, typename T2>
+vector<T1> operator*(const vector<T1>& u, const T2& c){
+	vector<T1> vec(u);
 	vec *= c;
 	return vec;
 }
 
 /* XXX: It is assuming that * is commutative  */
-template <typename ELEMENT>
-vector<ELEMENT> operator*(const ELEMENT& c, const vector<ELEMENT>& v){
+template <typename T1, typename T2>
+vector<T1> operator*(const T1& c, const vector<T2>& v){
 	return v * c;
 }
 
-template <typename ELEMENT>
-vector<ELEMENT> product_component_by_component(const vector<ELEMENT>& u, const vector<ELEMENT>& v){
+template <typename T1, typename T2>
+vector<T1> product_component_by_component(const vector<T1>& u, const vector<T2>& v){
 	if (u.size() != v.size())
 		throw std::invalid_argument("It is impossible to multiply vectors of different sizes.");
 	unsigned int n = u.size();
-	vector<ELEMENT> resp;
+	vector<T1> resp;
 	for (unsigned int i = 0; i < n; i++){
 		resp.push_back(u[i] * v[i]);
 	}
